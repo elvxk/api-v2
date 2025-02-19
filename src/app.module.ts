f784f22bdc15fb550b0346bdba2318605f0cf241
elvxk <pradiptaramadan9c@gmail.com>
2025-02-20 00:24:19 +0700

    update(module): add products and users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ProjectsModule } from './projects/projects.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [PrismaModule, ProjectsModule],
+  imports: [PrismaModule, ProjectsModule, UserModule, ProductsModule],
   controllers: [AppController],
 })
 export class AppModule {}
