8ada24aaf14f91225ed34cf0a9c0326422254bf2
Pradipta R. Sandri <pradiptaramadan9c@gmail.com>
2025-06-03 09:48:01 +0700

    feat(endpoint): dns lookup endpoint (#5)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { ProductsModule } from './products/products.module';
 import { SmtpModule } from './smtp/smtp.module';
+import { LookupModule } from './lookup/lookup.module';
 
 @Module({
-  imports: [PrismaModule, ProjectsModule, UserModule, ProductsModule, SmtpModule],
+  imports: [PrismaModule, ProjectsModule, UserModule, ProductsModule, SmtpModule, LookupModule],
   controllers: [AppController],
 })
-export class AppModule {}
+export class AppModule { }
