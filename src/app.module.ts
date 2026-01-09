c4e76f0925a5da8387146af14994da0328d9d99e
Pradipta R. Sandri <pradiptaramadan9c@gmail.com>
2026-01-10 02:56:49 +0700

    Fix/update (#8)
    * fix(update): update package
    
    * remove(endpoint): remove products dummy endpoint
    
    * remove(endpoint): remove user dummy endpoint
    
    * fix(module): remove products and user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ProjectsModule } from './projects/projects.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { UserModule } from './user/user.module';
-import { ProductsModule } from './products/products.module';
 import { SmtpModule } from './smtp/smtp.module';
 import { LookupModule } from './lookup/lookup.module';
 
 @Module({
-  imports: [PrismaModule, ProjectsModule, UserModule, ProductsModule, SmtpModule, LookupModule],
+  imports: [PrismaModule, ProjectsModule, SmtpModule, LookupModule],
   controllers: [AppController],
 })
 export class AppModule { }
