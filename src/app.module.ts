96852e7c68bafa655e58e4ea6f07d28ad9bd797b
Pradipta R. Sandri <pradiptaramadan9c@gmail.com>
2026-01-12 03:51:29 +0700

    Feat/acme (#9)
    * feat(update): update package
    
    * feat(acme): add ssl service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { ProjectsModule } from './projects/projects.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { SmtpModule } from './smtp/smtp.module';
 import { LookupModule } from './lookup/lookup.module';
+import { SslModule } from './ssl/ssl.module';
 
 @Module({
-  imports: [PrismaModule, ProjectsModule, SmtpModule, LookupModule],
+  imports: [PrismaModule, ProjectsModule, SmtpModule, LookupModule, SslModule],
   controllers: [AppController],
 })
 export class AppModule { }
